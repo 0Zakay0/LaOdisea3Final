b127fbc900a43f8c9e7f46ca4c9e59e408b6ccb8
Matias <matiasplaza312003@mail.com>
2023-11-20 14:17:53 -0300

    commit Avance

--- Action RPG Resources/Enemies/hadita.gd
+++ Action RPG Resources/Enemies/hadita.gd
@@ -3,11 +3,24 @@ extends KinematicBody2D
 export var speed = 100
 var player_position
 var target_position
+var original_position
+var player_detected = false
 onready var player = get_parent().get_node("Player")
+
+func _ready():
+	
+	original_position = position
 
 func _physics_process(delta):
 	player_position = player.position
 	target_position = (player_position - position).normalized()
-	if position.distance_to(player_position)<115:
-		move_and_slide(target_position *speed)
+
+	if position.distance_to(player_position) < 115:
 		
+		move_and_slide(target_position * speed)
+		player_detected = true
+	else:
+		player_detected = false
+	if not player_detected:
+		var direction_to_original = (original_position - position).normalized()
+		move_and_slide(direction_to_original * speed)
